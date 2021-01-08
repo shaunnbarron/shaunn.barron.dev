06e8b086f53c5ad8c4a071c9254586e7206890b7
Shaunn Barron <shaunn@barron.email>
2021-01-08 00:07:56 -0500

    initial commit

---
+++ components/SectionTitle.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="section-title">
+      <div class="text-left text-3xl mb-2">{{title}}</div>
+      <hr class="mb-4">
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "SectionTitle",
+    props: {
+      title: {
+        type: String,
+        required: true
+      }
+    }
+  };
+
+</script>
+
+<style scoped>
+  hr {
+    display: block;
+    height: 1px;
+    border: 0;
+    border-top: 1px solid #ccc;
+    margin: 1em 0;
+    padding: 0;
+  }
+</style>
