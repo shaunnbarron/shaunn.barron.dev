ad8b343e30d536e2d73e60d052c1c76f3fe31e86
pkgmain <blazer.forks0y@icloud.com>
2024-01-30 21:13:28 -0500

    bumped nuxt and added nuxt devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devtools: {enabled: true},
   modules: [
     "@nuxt/content",
+    "@nuxt/devtools",
     "@nuxt/image",
     "@nuxtjs/color-mode",
     "@nuxtjs/tailwindcss",
