b1038257a35c822e40ae18c64e44754aa772af27
pkgmain <blazer.forks0y@icloud.com>
2024-05-21 21:58:39 -0400

    replaced heroicons with local components

--- components/ThemeToggle.vue
+++ components/ThemeToggle.vue
@@ -1,21 +1,19 @@
 <script setup lang="ts">
-import {MoonIcon, SunIcon} from "@heroicons/vue/24/outline";
-
 const toggledValue: Record<string, string> = {
   dark: "light",
-  light: "dark"
-}
+  light: "dark",
+};
 
-const colorMode = useColorMode()
+const colorMode = useColorMode();
 function toggle() {
-  colorMode.preference = toggledValue[colorMode.value]
+  colorMode.preference = toggledValue[colorMode.value];
 }
 </script>
 
 <template>
   <button @click="toggle">
-    <MoonIcon v-show="colorMode.value === 'dark'"/>
-    <SunIcon v-show="colorMode.value === 'light'"/>
-    <span class="sr-only">{{colorMode.value}} mode</span>
+    <MoonIcon v-show="colorMode.value === 'dark'" />
+    <SunIcon v-show="colorMode.value === 'light'" />
+    <span class="sr-only">{{ colorMode.value }} mode</span>
   </button>
 </template>
