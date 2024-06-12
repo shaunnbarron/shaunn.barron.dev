8766f9426ecf84ed102601f296a9135f70ebb187
pkgmain <blazer.forks0y@icloud.com>
2024-06-11 20:56:31 -0400

    bumped nuxt to 3.12.1

--- components/ThemeToggle.vue
+++ components/ThemeToggle.vue
@@ -1,19 +1,14 @@
 <script setup lang="ts">
-const toggledValue: Record<string, string> = {
-  dark: "light",
-  light: "dark",
-};
-
 const colorMode = useColorMode();
 function toggle() {
-  colorMode.preference = toggledValue[colorMode.value];
+    colorMode.value === 'dark' ? colorMode.preference = 'light' : colorMode.preference = 'dark';
 }
 </script>
 
 <template>
-  <button @click="toggle">
-    <MoonIcon v-show="colorMode.value === 'dark'" />
-    <SunIcon v-show="colorMode.value === 'light'" />
-    <span class="sr-only">{{ colorMode.value }} mode</span>
-  </button>
+    <button @click="toggle" title="Toggle theme">
+        <MoonIcon v-show="colorMode.value === 'dark'" />
+        <SunIcon v-show="colorMode.value === 'light'" />
+        <span class="sr-only">{{ colorMode.value }} mode</span>
+    </button>
 </template>
