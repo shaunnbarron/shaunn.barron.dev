b9bf5f61cb74b444e1b7bf04e689797e840c3056
pkgmain <blazer.forks0y@icloud.com>
2024-07-02 20:21:24 -0400

    bumped to nuxt 3.12.3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: {enabled: true},
+  compatibilityDate: "2024-07-02",
+
   colorMode: {
     classSuffix: "",
   },
+
+  devtools: {enabled: true},
+
   future: {
     compatibilityVersion: 4,
   },
+
   modules: [
     "@nuxt/devtools",
     "@nuxt/image",
     "@nuxtjs/color-mode",
     "@nuxtjs/tailwindcss",
   ],
+
   vue: {
     propsDestructure: true,
-  }
+  },
 });
