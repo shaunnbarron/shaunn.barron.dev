4b7d5db010219d954c81925da5694ae9fe82b2e9
pkgmain <blazer.forks0y@icloud.com>
2024-07-17 20:28:54 -0400

    chore: Update dependencies and fix section title heading level
    - Update dependencies to the latest stable versions
    - Fix section title heading level from h1 to h2 in SectionTitle component

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useHead({
+  title: "Shaunn Barron",
+  htmlAttrs: {
+    lang: "en"
+  },
+})
+</script>
+
 <template>
   <main
     class="flex flex-wrap justify-center p-4 text-gray-600 dark:bg-gray-800 dark:text-gray-200"
--- app/components/SectionTitle.vue
+++ app/components/SectionTitle.vue
@@ -4,7 +4,7 @@ defineProps<{ id: string; title: string }>();
 
 <template>
   <div class="section-title">
-    <h1 :id class="mb-2 text-left text-3xl">{{ title }}</h1>
+    <h2 :id class="mb-2 text-left text-3xl">{{ title }}</h2>
     <hr
       class="my-[1em] mx-0 mb-4 block h-[1px] border-0 border-t border-t-[#ccc] p-0"
     />
